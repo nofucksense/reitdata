a59298d2a162f46d5034730885ada540be05e251
wernergiam <wernergiam73@gmail.com>
2016-12-30 10:29:59 +0800

    Dun use ng-app to bootstrap the application to angular. Use manual bootstrapping. Use module to segregate the application. Good for big application.

--- reit_data_app.js
+++ reit_data_app.js
@@ -2,11 +2,7 @@ var app = angular.module('reitDataApp', []);
 
 app.controller('reitDataCtrl', ['$scope', '$window', function($scope, $window) {
     $scope.reitDatas = $window.reitDatas;
-    $scope.otherDatas = $window.otherDatas;
-    $scope.shippingDatas = $window.shippingDatas;
     $scope.sortBy = "";
-    $scope.sortByOther = "";
-    $scope.sortByShipping = "";
     $scope.debugMode = true;
   
     $scope.init = function() {
@@ -35,3 +31,77 @@ app.controller('reitDataCtrl', ['$scope', '$window', function($scope, $window) {
     };
    
 }]);
+
+var appShipping = angular.module('shippingDataApp', []);
+
+appShipping.controller('shippingDataCtrl', ['$scope', '$window', function($scope, $window) {
+    $scope.shippingDatas = $window.shippingDatas;
+    $scope.sortBy = "";
+    $scope.debugMode = true;
+  
+    $scope.init = function() {
+        console.log("angular scope: " + $scope.shippingDatas.length);
+    };
+    
+    $scope.displayYield = function(shippingData) {
+        var ttlDpu = 0;
+        var prevClosed = 0;
+        
+        if (!Number.isNaN(shippingData.ttlDpu)) {
+            ttlDpu = parseFloat(shippingData.ttlDpu);
+        }
+            
+        if (!Number.isNaN(shippingData.prevClosed)) {
+            prevClosed = parseFloat(shippingData.prevClosed);
+        }
+           
+        if (ttlDpu > 0) {
+            if (prevClosed > 0) {
+                return (ttlDpu / prevClosed).toFixed(3) + "%";
+            } 
+        } 
+        
+        return shippingData.yield;
+    };
+   
+}]);
+
+var appOther = angular.module('otherDataApp', []);
+
+appOther.controller('otherDataCtrl', ['$scope', '$window', function($scope, $window) {
+    $scope.otherDatas = $window.otherDatas;
+    $scope.sortBy = "";
+    $scope.debugMode = true;
+  
+    $scope.init = function() {
+        console.log("angular scope: " + $scope.otherDatas.length);
+    };
+    
+    $scope.displayYield = function(otherData) {
+        var ttlDpu = 0;
+        var prevClosed = 0;
+        
+        if (!Number.isNaN(otherData.ttlDpu)) {
+            ttlDpu = parseFloat(otherData.ttlDpu);
+        }
+            
+        if (!Number.isNaN(otherData.prevClosed)) {
+            prevClosed = parseFloat(otherData.prevClosed);
+        }
+           
+        if (ttlDpu > 0) {
+            if (prevClosed > 0) {
+                return (ttlDpu / prevClosed).toFixed(3) + "%";
+            } 
+        } 
+        
+        return otherData.yield;
+    };
+   
+}]);
+
+angular.element(document).ready(function() {
+    angular.bootstrap(document.getElementById("appReit"), ['reitDataApp']);
+    angular.bootstrap(document.getElementById("appOther"), ['otherDataApp']);
+    angular.bootstrap(document.getElementById("appShipping"), ['shippingDataApp']);
+});
