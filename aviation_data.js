a6ee448d85c19efe656ee6a427c60a293835f2f5
wernergiam <wernergiam73@gmail.com>
2017-01-11 11:59:21 +0800

    Refresh the data javascript with new data from server.

--- aviation_data.js
+++ aviation_data.js
@@ -1,5 +1,5 @@
 var aviationDatas = [
-{ "code": "S58", "descr": "SATS", "prevClosed": "4.900", "period": "FY16 (Mar)", "dpu": "15.000", "ttlDpu": "0.000", "yield": "3.093%", "epsCts": "19.90", "pe": "24.37", "divBreakdown": "Interim 5ct ; Final 10ct" }
-, { "code": "S59", "descr": "SIA Engineering", "prevClosed": "3.400", "period": "FY16 (Mar)", "dpu": "14.000", "ttlDpu": "0.000", "yield": "4.154%", "epsCts": "15.74", "pe": "21.41", "divBreakdown": "Interim 6ct ; Final 8ct" }
-, { "code": "S63", "descr": "ST Engineering", "prevClosed": "3.300", "period": "FY15 (Dec)", "dpu": "15.000", "ttlDpu": "0.000", "yield": "4.644%", "epsCts": "17.05", "pe": "18.94", "divBreakdown": "Interim 5ct ; Final 5ct + Special 5ct" }
+{ "code": "S58", "descr": "SATS", "prevClosed": "4.940", "period": "FY16 (Mar)", "dpu": "15.000", "ttlDpu": "15.000", "yield": "3.093%", "epsCts": "19.90", "pe": "24.37", "divBreakdown": "Interim 5ct ; Final 10ct" }
+, { "code": "S59", "descr": "SIA Engineering", "prevClosed": "3.410", "period": "FY16 (Mar)", "dpu": "14.000", "ttlDpu": "14.000", "yield": "4.154%", "epsCts": "15.74", "pe": "21.41", "divBreakdown": "Interim 6ct ; Final 8ct" }
+, { "code": "S63", "descr": "ST Engineering", "prevClosed": "3.360", "period": "FY15 (Dec)", "dpu": "15.000", "ttlDpu": "15.000", "yield": "4.644%", "epsCts": "17.05", "pe": "18.94", "divBreakdown": "Interim 5ct ; Final 5ct + Special 5ct" }
 ];
--- infra_data.js
+++ infra_data.js
@@ -1,3 +1,3 @@
 var infraDatas = [
-{ "code": "AZI", "descr": "AusNetServices", "prevClosed": "1.665", "period": "2H – Mar16", "dpu": "A4.265", "ttlDpu": "0.000", "yield": "5.428%", "nav": "A$0.82", "divBreakdown": "1H16 A4.265ct ; 2H16 A4.265ct" }
+{ "code": "AZI", "descr": "AusNetServices", "prevClosed": "1.680", "period": "2H - Mar16", "dpu": "A4.265", "ttlDpu": "8.99804", "yield": "5.428%", "nav": "A$0.82", "divBreakdown": "1H16 A4.265ct ; 2H16 A4.265ct" }
 ];
