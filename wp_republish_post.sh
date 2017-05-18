4361c5f5d3e30f4e68ae6f88331b5a4707b22676
wernergiam <wernergiam73@gmail.com>
2017-05-18 14:23:31 +0800

    include the table on wp_2_posts for yieldstocks.reitdata.com

--- wp_republish_post.sh
+++ wp_republish_post.sh
@@ -1,19 +1,21 @@
-COMMAND=`cat << EndOfMessage
-update wp_posts
-set post_date = now(),
-post_date_gmt = date_add(now(), interval -8 hour),
-post_modified = now(),
-post_modified_gmt = date_add(now(), interval -8 hour),
-post_title = concat(date_format(now(), '%M %Y'), ' - updated ', now())
-where post_title like concat(date_format(now(), '%M %Y'), '%') and post_status = 'publish'
-limit 1
-EndOfMessage
-`
-
 WPCONFIG=/home/reitdat1/www/wp-config.php
 
 USER=$(grep -Eo "DB_USER',.+'" $WPCONFIG | cut -d "'" -f 3)
 PASSWORD=$(grep -Eo "DB_PASSWORD',.+'" $WPCONFIG | cut -d "'" -f 3)
 DB=$(grep -Eo "DB_NAME',.+'" $WPCONFIG | cut -d "'" -f 3)
 
-mysql -u $USER --password=${PASSWORD} -e "$COMMAND" $DB
+for TABLE in `echo "wp_posts wp_2_posts"` ; do
+	echo $TABLE
+	COMMAND=`cat << EndOfMessage
+	update $TABLE
+	set post_date = now(),
+	post_date_gmt = date_add(now(), interval -8 hour),
+	post_modified = now(),
+	post_modified_gmt = date_add(now(), interval -8 hour),
+	post_title = concat(date_format(now(), '%M %Y'), ' - updated ', now())
+	where post_title like concat(date_format(now(), '%M %Y'), '%') and post_status = 'publish'
+	limit 1
+EndOfMessage
+`
+	mysql -u $USER --password=${PASSWORD} -e "$COMMAND" $DB
+done
